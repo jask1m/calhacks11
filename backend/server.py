a512d3eafcaa3a6736edd8fe195905aa349a845c
RonCodes88 <ronaldliyh@gmail.com>
2024-10-18 18:08:43 -0700

    Added backend

---
+++ backend/server.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import uvicorn
+import random
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return({"message": "hello word"})
+
+if __name__ == "main":
+    uvicorn.run("server:app", port=8000, reload=True)
+
+@app.get("/flip-coin")
+def flipcoin():
+    val = random.random()
+    head_tail = ""
+    if(val <= 0.5):
+        head_tail = "heads"
+    else:
+        head_tail = "tails"
+    return({ "value": head_tail})
+
+@app.get("/flip-coins")
+def flip_coins(times: int):
+    if times and times > 0:
+        head_count = 0
+        tail_count = 0
+        for _ in range(times):
+            val = random.random()
+            if(val <= 0.5):
+                head_count += 1
+            else:
+                tail_count += 1
+        return ({ "heads": head_count, "tails": tail_count})
+    else:
+        return({ "message": "you need to send valid times"})
