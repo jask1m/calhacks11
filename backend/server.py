777266119ecf559ba38f4894f2d61b4e37dfbd73
RonCodes88 <ronaldliyh@gmail.com>
2024-10-18 18:44:41 -0700

    server changes

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 import uvicorn
 import random
 
+
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
